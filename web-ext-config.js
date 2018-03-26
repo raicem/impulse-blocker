3c6b208dc3b1e329b04e58763131b67011a91560
Cem Ünalan <unalancem@gmail.com>
2018-03-26 09:16:06 +0300

    Added build script and configuration

---
+++ web-ext-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  ignoreFiles: [
+    'package.json',
+    'package-lock.json',
+    '.eslintrc',
+    '.gitignore',
+    'LICENCE',
+    'web-ext-config.js',
+  ],
+  build: {
+    overwriteDest: true,
+  },
+};
