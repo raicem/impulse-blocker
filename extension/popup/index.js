97b27984e77d96a3cb6c6a121e1300a8aba2355b
raicem <unalancem@gmail.com>
2019-01-13 13:46:12 +0300

    Changed the line endings

--- extension/popup/index.js
+++ extension/popup/index.js
@@ -4,38 +4,44 @@ import './popup.css';
 import cogs from './cogs.svg';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <header>
-                    <h5 class="title">Impulse Blocker</h5>
-                    <img class="options" src={cogs} />
-                </header>
-                <main>
-                    <form action="GET">
-                        <div class="form-group">
-                            <input type="radio" name="status" id="on" value="on" />
-                            <label for="on">On</label >
-                        </div>
-                        <div class="form-group">
-                            <input type="radio" name="status" id="off" value="off" />
-                            <label for="off">Off</label >
-                        </div>
-                    </form>
-                    <div class="add-domain-section">
-                        <button class="button button-add">
-                            Block
-                    <span class="domainToAllow"></span>
-                        </button>
-                        <button class="button button-remove">
-                            Allow
-                    <span class="domainToBlock"></span>
-                        </button>
-                    </div>
-                </main>
+  constructor() {
+    super();
+    this.state = {
+      extensionStatus: 'on',
+      domain: '',
+      isBlocked: false,
+    };
+  }
+
+  componentDidMount() {
+    console.log('mounted');
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h5 className="title">Impulse Blocker</h5>
+          <img className="options" src={cogs} />
+        </header>
+        <main>
+          <form action="GET">
+            <div className="form-group">
+              <input type="radio" name="status" id="on" value="on" />
+              <label htmlFor="on">On</label>
             </div>
-        );
-    }
+            <div className="form-group">
+              <input type="radio" name="status" id="off" value="off" />
+              <label htmlFor="off">Off</label>
+            </div>
+          </form>
+          <div className="add-domain-section">
+            <button className="button button-add">{this.state.domain}</button>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
