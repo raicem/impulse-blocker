a3af857dfe48c812384f92830aa7b714225d6054
raicem <unalancem@gmail.com>
2019-02-21 21:48:32 +0300

    Remove html-webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
